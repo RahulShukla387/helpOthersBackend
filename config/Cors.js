50c9132aba1c9fb128ef0ff8599999516156f616
RahulShukla387 <KnowMeTechnical@gmail.com>
2026-03-14 00:48:20 +0530

    adding origin again

--- config/Cors.js
+++ config/Cors.js
@@ -1,12 +1,14 @@
 
- 
-let UseCors =()=>{
-     return {
-        // origin: "http://localhost:5173",
-        origin: "https://myprojectadiyuvanbackend-production.up.railway.app",
-        methods: ["GET", "POST", "PATCH", "PUT", "DELETE" ],
-         allowedHeaders: ["Content-Type", "Authorization"],
-        credentials: true
-    }
+let UseCors = () => {
+  return {
+    origin: [
+      "http://localhost:5173",
+      "https://helpothers-five.vercel.app"
+    ],
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+  }
 }
-export {UseCors}
+
+export { UseCors }
